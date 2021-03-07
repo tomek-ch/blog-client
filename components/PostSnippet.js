6cbcb2a2906d3bc906bf535c8e9ea49b2188b91c
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-03-07 14:54:36 +0100

    Remove paragraph heading from post snippet

--- components/PostSnippet.js
+++ components/PostSnippet.js
@@ -3,13 +3,10 @@ import { postSnippet } from '../styles/PostSnippet.module.css';
 
 function PostThumbnail({ post: { _id, title, paragraphs } }) {
 
-    const heading = paragraphs[0].heading ? <h3>{paragraphs[0].heading}</h3> : '';
-
     return (
         <Link href={`/posts/${_id}`}>
             <a className={postSnippet}>
                 <h2>{title}</h2>
-                {heading}
                 <p>{paragraphs[0].body}</p>
             </a>
         </Link>
