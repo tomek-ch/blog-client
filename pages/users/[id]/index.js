3f0b7206d55f51828d23ad57987e6ad73a502938
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-03-10 19:07:28 +0100

    Display post snippets on user profile

--- pages/users/[id]/index.js
+++ pages/users/[id]/index.js
@@ -1,7 +1,10 @@
 import Meta from '../../../components/Meta';
+import Tags from '../../../components/Tags';
+import Details from '../../../components/Details';
 import { container } from '../../../styles/Profile.module.css';
+import Link from 'next/link';
 
-function Post({ user, error }) {
+function Post({ user, posts, error }) {
 
     if (error) return (
         <div className={container}>
@@ -13,8 +16,19 @@ function Post({ user, error }) {
     return (
         <div className={container}>
             <Meta title={`${user.firstName} - Blogg`} />
-            <h1>{user.firstName}</h1>
-
+            <h1>{user.firstName} {user.lastName}</h1>
+            {posts.map(post => (
+                <div key={post.id}>
+                    <Link href={`/posts/${post._id}`}>
+                        <a>
+                            <h2>{post.title}</h2>
+                        </a>
+                    </Link>
+                    <Details post={post} />
+                    <p>{post.paragraphs[0].body}</p>
+                    <Tags tags={post.tags} />
+                </div>
+            ))}
         </div>
     );
 }
@@ -24,7 +38,7 @@ export async function getServerSideProps({ params: { id } }) {
         const res = await fetch(`http://localhost:5000/users/${id}`);
         const data = await res.json();
 
-        if (res.status === 200) return { props: { user: data } };
+        if (res.status === 200) return { props: data };
         else return { props: { error: data[0] } };
 
     } catch (error) {
