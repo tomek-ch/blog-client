78234f48ea0124cdd436a4921477ee5db1a422f1
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-03-24 20:00:20 +0100

    Restructure comment layout

--- components/Comment.js
+++ components/Comment.js
@@ -1,14 +1,15 @@
-import { comment, details } from '../styles/Comment.module.css';
+import { comment, details, body } from '../styles/Comment.module.css';
 import Link from 'next/link';
 
 function Comment({ text, time, author }) {
     return (
         <div className={comment}>
-            <Link href={`/users/${author._id}`}>
-                <a className={details}>{author.firstName} {author.lastName}</a>
-            </Link>
-            <div>{text}</div>
-            <div className={details}>{time}</div>
+            <div className={details}>
+                <Link href={`/users/${author._id}`}>
+                    <a className={details}>{author.firstName} {author.lastName}</a>
+                </Link> • {time}
+            </div>
+            <div className={body}>{text}</div>
         </div>
     );
 }
