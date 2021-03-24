cce1e5168dd36ce126aac3c28f5bc48cb0c9be4d
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-03-24 19:51:47 +0100

    Link to comment authors

--- components/Comment.js
+++ components/Comment.js
@@ -1,9 +1,12 @@
 import { comment, details } from '../styles/Comment.module.css';
+import Link from 'next/link';
 
 function Comment({ text, time, author }) {
     return (
         <div className={comment}>
-            <div className={details}>{author.firstName} {author.lastName}</div>
+            <Link href={`/users/${author._id}`}>
+                <a className={details}>{author.firstName} {author.lastName}</a>
+            </Link>
             <div>{text}</div>
             <div className={details}>{time}</div>
         </div>
