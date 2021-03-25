ed9ae8cee63cc1c5527e2ad32831dc2b40e7089c
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-03-25 20:46:54 +0100

    Style comment form

--- components/CommentForm.js
+++ components/CommentForm.js
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { input } from '../styles/Form.module.css';
+import { btn } from '../styles/Btn.module.css';
+import { form } from '../styles/CommentForm.module.css';
 
 function CommentForm({ handleSubmit }) {
 
@@ -7,18 +10,21 @@ function CommentForm({ handleSubmit }) {
 
     return (
         <>
-            <form onSubmit={e => {
-                e.preventDefault();
-                handleSubmit(comment, setError, () => setComment(''));
-            }}>
+            <form
+                className={form}
+                onSubmit={e => {
+                    e.preventDefault();
+                    handleSubmit(comment, setError, () => setComment(''));
+                }}>
                 <input
                     placeholder="Write a comment"
                     onChange={e => setComment(e.target.value)}
                     value={comment}
+                    className={input}
                 />
-                <button>
+                <button className={btn}>
                     Add
-            </button>
+                </button>
             </form>
             <div>{error}</div>
         </>
