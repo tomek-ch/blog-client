cce62f53f2d11c5a09b432cfdbee803247c5a23e
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-03-30 16:56:21 +0200

    Extract reply editor to separate component

--- components/Comment.js
+++ components/Comment.js
@@ -1,48 +1,13 @@
 import { comment, body } from '../styles/Comment.module.css';
-import { input } from '../styles/Form.module.css';
-import { btn } from '../styles/Btn.module.css';
 import { useState } from 'react';
-import TextBox from './TextBox';
 import CommentDetails from './CommentDetails';
 import CommentEditor from './CommentEditor';
+import ReplyEditor from './ReplyEditor';
 
 function Comment({ replies, _id, text, time, author, editable, setComments, token }) {
 
     const [isEdited, setIsEdited] = useState(false);
     const [error, setError] = useState('');
-
-    const [isReplyOpen, setIsReplyOpen] = useState(false);
-    const [replyText, setReplyText] = useState('');
-
-    const addReply = async () => {
-        try {
-            const res = await fetch('http://localhost:5000/replies', {
-                method: 'post',
-                body: JSON.stringify({ text: replyText, comment: _id }),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`,
-                },
-            });
-
-            if (res.status === 200) {
-                const reply = await res.json();
-                setComments(prev => prev.map(com => com._id === _id ? {
-                    ...com,
-                    replies: [...com.replies, reply],
-                } : com));
-
-                setError('');
-                setIsReplyOpen(false);
-                setReplyText('');
-            } else {
-                const data = await res.json();
-                setError(data[0]);
-            }
-        } catch (e) {
-            setError('Error trying to submit');
-        }
-    };
 
     return (
         <div className={comment}>
@@ -52,15 +17,7 @@ function Comment({ replies, _id, text, time, author, editable, setComments, toke
                     ? <CommentEditor {...{ author, _id, token, setComments, text, setError, setIsEdited }} />
                     : <div className={body}>{text}</div>
             }
-            <button onClick={() => setIsReplyOpen(prev => !prev)}>Reply</button>
-            {isReplyOpen && <div>
-                <TextBox
-                    className={input}
-                    value={replyText}
-                    onChange={e => setReplyText(e.target.value)}
-                />
-                <button className={btn} onClick={addReply} disabled={!replyText}>Add</button>
-            </div>}
+            <ReplyEditor {...{ _id, token, setComments, setError }} />
             <div>{error}</div>
             {replies.map(rep => <div key={rep._id}>{rep.text}</div>)}
         </div>
---
+++ components/ReplyEditor.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import TextBox from './TextBox';
+import { input } from '../styles/Form.module.css';
+import { btn } from '../styles/Btn.module.css';
+
+function ReplyEditor({ _id, token, setComments, setError }) {
+
+    const [isReplyOpen, setIsReplyOpen] = useState(false);
+    const [replyText, setReplyText] = useState('');
+
+    const addReply = async () => {
+        try {
+            const res = await fetch('http://localhost:5000/replies', {
+                method: 'post',
+                body: JSON.stringify({ text: replyText, comment: _id }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+
+            if (res.status === 200) {
+                const reply = await res.json();
+                setComments(prev => prev.map(com => com._id === _id ? {
+                    ...com,
+                    replies: [...com.replies, reply],
+                } : com));
+
+                setError('');
+                setIsReplyOpen(false);
+                setReplyText('');
+            } else {
+                const data = await res.json();
+                setError(data[0]);
+            }
+        } catch (e) {
+            setError('Error trying to submit');
+        }
+    };
+
+    return (
+        <>
+            <button onClick={() => setIsReplyOpen(prev => !prev)}>Reply</button>
+            {
+                isReplyOpen && <div>
+                    <TextBox
+                        className={input}
+                        value={replyText}
+                        onChange={e => setReplyText(e.target.value)}
+                    />
+                    <button className={btn} onClick={addReply} disabled={!replyText}>Add</button>
+                </div>
+            }
+        </>
+    );
+}
+
+export default ReplyEditor;
