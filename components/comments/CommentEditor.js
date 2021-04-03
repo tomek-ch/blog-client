3b459b08c33066ec344b49a37a9e4705618bdc1a
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-04-03 18:29:08 +0200

    Move comment update function to separate file

--- components/comments/CommentEditor.js
+++ components/comments/CommentEditor.js
@@ -2,47 +2,19 @@ import { input } from '../../styles/Form.module.css';
 import { btn } from '../../styles/Btn.module.css';
 import { useState } from 'react';
 import TextBox from '../TextBox';
+import updateComment from './api/commentUpdate';
 
-function CommentEditor({ comment: { _id, author, text, replies }, token, setComments, setError, setIsEdited }) {
+function CommentEditor({ comment, token, setComments, setError, setIsEdited }) {
 
-    const [editedText, setEditedText] = useState(text);
+    const [editedText, setEditedText] = useState(comment.text);
 
     const cancelEdit = () => {
         setIsEdited(false);
-        setEditedText(text);
+        setEditedText(comment.text);
         setError('');
     };
 
-    const updateComment = async () => {
-        try {
-            const res = await fetch(`http://localhost:5000/comments/${_id}`, {
-                method: 'put',
-                body: JSON.stringify({ text: editedText }),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`,
-                },
-            });
-
-            if (res.status === 200) {
-                const updatedComment = await res.json();
-                setComments(prev => prev.map(com => com._id === _id ? {
-                    ...updatedComment,
-                    author,
-                    replies,
-                } : com));
-
-                setError('');
-                setIsEdited(false);
-            } else {
-                const data = await res.json();
-                setError(data[0]);
-            }
-        } catch (e) {
-            console.log(e);
-            setError('Error trying to submit');
-        }
-    };
+    const handleUpdate = () => updateComment(comment, editedText, token, setComments, setError, setIsEdited);
 
     return (
         <div>
@@ -51,7 +23,7 @@ function CommentEditor({ comment: { _id, author, text, replies }, token, setComm
                 value={editedText}
                 onChange={e => setEditedText(e.target.value)}
             />
-            <button className={btn} onClick={updateComment}>Save</button>
+            <button className={btn} onClick={handleUpdate}>Save</button>
             <button className={btn} onClick={cancelEdit}>Cancel</button>
         </div>
     );
---
+++ components/comments/api/commentUpdate.js
@@ -0,0 +1,31 @@
+async function updateComment(comment, editedText, token, setComments, setError, setIsEdited) {
+    try {
+        const res = await fetch(`http://localhost:5000/comments/${comment._id}`, {
+            method: 'put',
+            body: JSON.stringify({ text: editedText }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+
+        if (res.status === 200) {
+            const updatedComment = await res.json();
+            setComments(prev => prev.map(com => com._id === comment._id ? {
+                ...updatedComment,
+                replies: comment.replies,
+                author: comment.author,
+            } : com));
+
+            setError('');
+            setIsEdited(false);
+        } else {
+            const data = await res.json();
+            setError(data[0]);
+        }
+    } catch {
+        setError('Error trying to submit');
+    }
+};
+
+export default updateComment;
