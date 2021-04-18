f50f5834ffa308f9f4f394ecf5122c49014793bc
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-04-18 13:51:03 +0200

    Handle search bar hide

--- components/header/SearchBar.js
+++ components/header/SearchBar.js
@@ -1,11 +1,36 @@
 import { useEffect, useRef, useState } from 'react';
 import style, { btn, bar, container, fullWidthContainer } from '../../styles/SearchBar.module.css';
+import Link from 'next/link';
 
 function SearchBar() {
 
     const [query, setQuery] = useState('');
     const [results, setResults] = useState([]);
+    const [areResultsVisible, setAreResultsVisible] = useState(false);
     const [cancelId, setCancelId] = useState(null);
+
+    const showResults = () => setAreResultsVisible(true);
+    const hideResults = () => setAreResultsVisible(false);
+
+    const input = useRef(null);
+    const [isBarFullWidth, setIsBarFullWidth] = useState(false);
+
+    const toggleBar = async () => {
+        await setIsBarFullWidth(prev => !prev);
+        input.current?.focus();
+    };
+
+    const handleBlur = e => {
+        if (!e.relatedTarget?.dataset.result) {
+            hideResults();
+            setIsBarFullWidth(false);
+        }
+    };
+
+    const handleResultClick = () => {
+        hideResults();
+        setIsBarFullWidth(false);
+    };
 
     useEffect(() => {
         if (cancelId !== null)
@@ -30,15 +55,6 @@ function SearchBar() {
         return () => clearTimeout(cancelId);
     }, [query]);
 
-
-    const input = useRef(null);
-    const [isBarFullWidth, setIsBarFullWidth] = useState(false);
-
-    const toggleBar = async () => {
-        await setIsBarFullWidth(prev => !prev);
-        input.current?.focus();
-    };
-
     return (
         <>
             <div className={isBarFullWidth ? fullWidthContainer : container}>
@@ -46,14 +62,22 @@ function SearchBar() {
                     placeholder="Search Blogg"
                     value={query}
                     onChange={e => setQuery(e.target.value)}
+                    onFocus={showResults}
+                    onBlur={handleBlur}
                     className={bar}
                     ref={input}
                 />
                 <button className={btn} onClick={toggleBar}>🔎</button>
                 {
-                    !!results.length &&
+                    areResultsVisible && !!results.length &&
                     <div className={style.results}>
-                        {results.map(user => <div key={user._id}>{user.username}</div>)}
+                        {results.map(user => (
+                            <Link key={user._id} href={`/users/${user._id}`}>
+                                <a onClick={handleResultClick} data-result>
+                                    {user.username}
+                                </a>
+                            </Link>
+                        ))}
                     </div>
                 }
             </div>
