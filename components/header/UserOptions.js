533947822184e9e6157f58c9f253cf9ef412a7ba
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-05-02 18:56:16 +0200

    Remove icons from user options

--- components/header/UserOptions.js
+++ components/header/UserOptions.js
@@ -19,30 +19,25 @@ function UserOptions({ signOut, currentUser }) {
                 <div className={options}>
                     <Link href="/new">
                         <a onClick={toggle} className={option} data-user>
-                            <i className="ri-pencil-line"></i>
                             New post
                         </a>
                     </Link>
                     <Link href={`/users/${currentUser.username}`}>
                         <a onClick={toggle} className={option} data-user>
-                            <i className="ri-user-3-line" />
                             Profile
                         </a>
                     </Link>
                     <Link href="/settings">
                         <a onClick={toggle} className={option} data-user>
-                            <i className="ri-settings-3-line"></i>
                             Settings
                         </a>
                     </Link>
                     <Link href="/your-posts">
                         <a onClick={toggle} className={option} data-user>
-                            <i className="ri-file-list-2-line"></i>
                             Your posts
                         </a>
                     </Link>
                     <button onClick={handleClick} className={option} data-user>
-                        <i className="ri-logout-box-r-line"></i>
                         Sign out
                     </button>
                 </div>
