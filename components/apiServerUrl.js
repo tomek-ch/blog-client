86e0ea764c319cb5c4de52af4689f72d3861c1b0
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-05-10 15:04:00 +0200

    Set api url based on environment

--- components/apiServerUrl.js
+++ components/apiServerUrl.js
@@ -1,1 +1,5 @@
-export default 'http://localhost:5000';
+const url = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000'
+    : 'https://lit-chamber-89885.herokuapp.com';
+
+export default url;
