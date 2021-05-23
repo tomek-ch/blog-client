5e66db9a45d9276ae86667bdb1f80da6a7b6e065
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-05-23 20:05:17 +0200

    Redesign post excerpt component

--- components/posts/PostExcerpt.js
+++ components/posts/PostExcerpt.js
@@ -1,19 +1,21 @@
 import Link from 'next/link';
 import Details from './Details';
-import Tags from './Tags';
+import { container, moreBtn, title } from '../../styles/PostExcerpt.module.css';
 
 function PostExcerpt({ post, setPosts }) {
 
     const handleDelete = () => setPosts(prev => prev.filter(p => p._id !== post._id));
 
     return (
-        <div key={post._id}>
-            <Link href={`/posts/${post._id}`}>
-                <a><h2>{post.title}</h2></a>
-            </Link>
+        <div key={post._id} className={container}>
             <Details {...{ post, handleDelete }} />
+            <Link href={`/posts/${post._id}`}>
+                <a><h2 className={title}>{post.title}</h2></a>
+            </Link>
             <p>{post.excerpt}</p>
-            <Tags tags={post.tags} />
+            <Link href={`/posts/${post._id}`}>
+                <a className={moreBtn}>Read more →</a>
+            </Link>
         </div>
     );
 }
