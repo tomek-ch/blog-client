8904239473b69af633e9131f77df1b559fd5eb41
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-05-24 15:29:05 +0200

    Style options toggle

--- components/Options.js
+++ components/Options.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { container } from '../styles/Options.module.css';
+import style, { container } from '../styles/Options.module.css';
 
-function Options({ toggleText, renderOptions, optionId }) {
+function Options({ toggleText, renderOptions, optionId, bold, square }) {
 
     const [optionsOpen, setOptionsOpen] = useState(false);
     const toggle = () => setOptionsOpen(prev => !prev);
@@ -10,11 +10,25 @@ function Options({ toggleText, renderOptions, optionId }) {
             setOptionsOpen(false);
     };
 
+    const toggleStyle = {};
+    if (optionsOpen)
+        toggleStyle.backgroundColor = 'var(--foreground)';
+
+    if (toggleText === '⋯') {
+        toggleStyle.width = '2em';
+        toggleStyle.height = '2em';
+        toggleStyle.fontWeight = 'bold';
+    } else {
+        toggleStyle.padding = '0.5em 1em';
+    }
+
     return (
         <div className={container}>
             <button
                 onClick={toggle}
                 onBlur={hideOptions}
+                className={style.toggle}
+                style={toggleStyle}
             >
                 {toggleText}
             </button>
