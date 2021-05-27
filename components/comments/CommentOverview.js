7ec956501dd097b9574e0d11f4ee13279450ac5f
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-05-27 23:53:14 +0200

    Limit comment overview length

--- components/comments/CommentOverview.js
+++ components/comments/CommentOverview.js
@@ -3,11 +3,15 @@ import Link from 'next/link';
 import CommentDetails from './CommentDetails';
 
 function CommentOverview({ com }) {
+    
+    const limitLength = (str, max) => str.length > max ? `${str.substring(0, max - 3)}...` : str;
+    const text = limitLength(com.text, 100);
+
     return (
         <div className={listItem}>
             <Link href={`/posts/${com.post ? com.post._id : com.comment.post}`}>
                 <a>
-                    <h3>{com.text}</h3>
+                    <h3>{text}</h3>
                     <p>
                         In response to {com.post ? 'post' : 'comment'}: {
                             com.post?.title || com.comment.text
