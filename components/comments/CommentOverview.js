7dcb5ad61a2e0c52093f735709cc1ecfa20e83c9
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-05-29 16:28:43 +0200

    Improve comment overview hierarchy

--- components/comments/CommentOverview.js
+++ components/comments/CommentOverview.js
@@ -1,20 +1,22 @@
-import { listItem } from '../../styles/YourPosts.module.css';
+import style from '../../styles/YourPosts.module.css';
 import Link from 'next/link';
 import CommentDetails from './CommentDetails';
 
 function CommentOverview({ com }) {
-    
+
     const limitLength = (str, max) => str.length > max ? `${str.substring(0, max - 3)}...` : str;
     const text = limitLength(com.text, 100);
     const responseTo = limitLength(com.post?.title || com.comment.text, 50);
 
     return (
-        <div className={listItem}>
+        <div className={style.listItem}>
             <Link href={`/posts/${com.post ? com.post._id : com.comment.post}`}>
                 <a>
-                    <h3>{text}</h3>
-                    <p>
-                        In response to {com.post ? 'post' : 'comment'}: {responseTo}
+                    <p className={style.comment}>{text}</p>
+                    <p className={style.details}>
+                        In response to {com.post ? 'post' : 'comment'}: <span className={style.responseTo}>
+                            {responseTo}
+                        </span>
                     </p>
                 </a>
             </Link>
