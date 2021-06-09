228052a9abc65528c3b141292d605cac78508ab8
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-06-09 20:35:54 +0200

    Add loading screen between route changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import { ContextProvider } from '../components/Context';
 import Header from '../components/header/Header';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
+
+    router.events.on('routeChangeStart', () => setIsLoading(true));
+    router.events.on('routeChangeComplete', () => setIsLoading(false));
+    router.events.on('routeChangeError', () => setIsLoading(false));
+
     return (
         <ContextProvider>
             <Header />
-            <Component {...pageProps} />
+            {
+                isLoading
+                    ? 'Loading...'
+                    : <Component {...pageProps} />
+            }
         </ContextProvider>
     );
 }
